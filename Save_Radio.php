e08d4742a2f20c9193e16b3185b6fe4698057cd8
CristianValero <152441965+CristianValero@users.noreply.github.com>
2024-02-06 11:08:09 -0500

    ac

--- Save_Radio.php
+++ Save_Radio.php
@@ -1,9 +1,26 @@
 <?php 
 
+include("Conexion.php");
+
   if (isset($_POST['Guardar'])) {
-    echo 'Guardado'
+    $Modelo = $_POST['Modelo'];
+    $Serial = $_POST['Serial'];
+    $Placa = $_POST['Placa'];
+    $bateria = $_POST['Bateria'];
+    $Cli = $_POST['Cli'];
+    $Cargador = $_POST['Cargador'];
+
+    $query = "INSERT INTO Radios(Modelo, Serial, Placa, Bateria, Cli, Cargador) VALUES ('$Modelo' , '$Serial', '$Placa', '$Bateria', '$Cli', '$Cargador')";
+    $result = mysqli_query($conn, $query);
+    if (!$result) {
+      die("Consulta cancelada");
+    }
   }
 
+  $_SESSION['message'] = 'Guardado Correctamente';
+  $_SESSION['message_type'] = 'success';
+  
+  header("Location: index.html");
 
 
 ?>
